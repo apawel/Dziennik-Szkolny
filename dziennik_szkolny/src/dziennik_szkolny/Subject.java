8aae27ac3c207ceb8b57e06bc9ff381f6a209e45
Paweł Wojciechowski <apawel1w@gmail.com>
2014-03-18 20:17:46 +0100

    Podstawowe Klasy

---
+++ dziennik_szkolny/src/dziennik_szkolny/Subject.java
@@ -0,0 +1,14 @@
+package dziennik_szkolny;
+
+public class Subject {
+	String name;
+
+	public Subject(String name) {		
+		this.name = name;
+	}
+
+	public Subject() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
