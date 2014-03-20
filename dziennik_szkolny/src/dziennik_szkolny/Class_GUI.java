229e27799b62e583151916802d0b2fcc4eb48e31
Paweł Wojciechowski <apawel1w@gmail.com>
2014-03-20 10:06:36 +0100

    Oienka dla nauczyciela i wodok calej klasy

---
+++ dziennik_szkolny/src/dziennik_szkolny/Class_GUI.java
@@ -0,0 +1,67 @@
+package dziennik_szkolny;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.JButton;
+
+public class Class_GUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Class_GUI frame = new Class_GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Class_GUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		table = new JTable();
+		table.setBounds(10, 24, 397, 197);
+		contentPane.add(table);
+		
+		JLabel lblKlasa = new JLabel("Klasa:");
+		lblKlasa.setBounds(10, 11, 46, 14);
+		contentPane.add(lblKlasa);
+		
+		JLabel lblNazwaklasy = new JLabel("nazwa_klasy");
+		lblNazwaklasy.setEnabled(false);
+		lblNazwaklasy.setBounds(81, 11, 69, 14);
+		contentPane.add(lblNazwaklasy);
+		
+		JButton btnWybierzUcznia = new JButton("Wybierz ucznia");
+		btnWybierzUcznia.setBounds(252, 227, 155, 23);
+		contentPane.add(btnWybierzUcznia);
+		
+		JButton btnWr = new JButton("Wr\u00F3\u0107");
+		btnWr.setBounds(20, 227, 130, 23);
+		contentPane.add(btnWr);
+	}
+}
---
+++ dziennik_szkolny/src/dziennik_szkolny/Teacher_GUI.java
@@ -0,0 +1,70 @@
+package dziennik_szkolny;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+
+public class Teacher_GUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Teacher_GUI frame = new Teacher_GUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Teacher_GUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 324, 150);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblPrzedmiot = new JLabel("Przedmiot");
+		lblPrzedmiot.setBounds(10, 11, 47, 20);
+		contentPane.add(lblPrzedmiot);
+		
+		JLabel lbl_prowadzonyprzeznauczycielaprzedmiot = new JLabel("prowadzony_przez_nauczyciela_przedmiot");
+		lbl_prowadzonyprzeznauczycielaprzedmiot.setEnabled(false);
+		lbl_prowadzonyprzeznauczycielaprzedmiot.setBounds(80, 14, 222, 14);
+		contentPane.add(lbl_prowadzonyprzeznauczycielaprzedmiot);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(90, 49, 164, 20);
+		contentPane.add(comboBox);
+		
+		JLabel lblKlasa = new JLabel("Klasa");
+		lblKlasa.setBounds(20, 52, 46, 14);
+		contentPane.add(lblKlasa);
+		
+		JButton btnDalej = new JButton("Dalej");
+		btnDalej.setBounds(212, 80, 89, 23);
+		contentPane.add(btnDalej);
+		
+		JButton btnWyloguj = new JButton("Wyloguj");
+		btnWyloguj.setBounds(10, 80, 89, 23);
+		contentPane.add(btnWyloguj);
+	}
+}
